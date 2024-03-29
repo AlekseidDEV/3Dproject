6f89ac8ce825fd0f6468beb4add8064a00cdcef5
Alex <lesha.rudiko@gmail.com>
2024-03-29 10:11:12 +0300

    dev branch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-import { timer } from "./modules/timer";
-import { modalMenu } from "./modules/menu";
-import { modalWind } from "./modules/modal";
-import { scrollLink } from "./modules/scrolllink";
 
-timer('1 april 2024')
-
-modalMenu()
-modalWind()
-scrollLink()
--- src/modules/smoothscroll.js
+++
@@ -1,8 +0,0 @@
-export const smoothScroll = (id) => {
-    const blockScroll = document.querySelector(id)
-    
-    blockScroll.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-    })
-}
