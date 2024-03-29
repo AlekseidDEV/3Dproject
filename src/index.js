ae92219124b186a4902f351991d9dd3b445abd08
Alex <lesha.rudiko@gmail.com>
2024-03-29 14:54:37 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import { timer } from "./modules/timer";
 import { modalMenu } from "./modules/menu";
 import { modalWind } from "./modules/modal";
 import { scrollLink } from "./modules/scrolllink";
+import { sliderFunc } from "./modules/slider";
 
 timer('1 april 2024')
 
 modalMenu()
 modalWind()
 scrollLink()
-// dev
+sliderFunc(['.portfolio-content', '.portfolio-item', 'portfolio-item-active', 'dot', 'dot-active', '.prev', '.next', 2000])
